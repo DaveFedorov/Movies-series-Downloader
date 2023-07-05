512028ae66809e586f37050d343eb9016b590df6
SheepMan <pepito.mussiloni@gmail.com>
2023-07-05 20:15:02 +0300

    added UI with a button to copy link

--- downloader.py
+++ downloader.py
@@ -1,20 +1,26 @@
 import json
 import pprint
 import tkinter as tk
+import clipboard
 from selenium import webdriver
+from tkinter import messagebox, ttk
+from tkinter import Tk, Text, Button, Label, Entry
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import re
 import time
+from datetime import datetime, timedelta
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
 
 capabilities = DesiredCapabilities.CHROME
 capabilities["goog:loggingPrefs"] = {"performance": "ALL"} 
 
-driver = webdriver.Chrome(
-    r"chromedriver.exe",
-    desired_capabilities=capabilities,
-)
+driver = webdriver.Chrome(r"chromedriver.exe", desired_capabilities=capabilities, options=chrome_options)
 network_log = []
+
 def process_browser_logs_for_network_events(logs):
     """
     Return only logs which have a method that start with "Network.response", "Network.request", or "Network.webSocket"
@@ -41,14 +47,45 @@ def find_url():
                 if index != -1:
                     mod_link = extracted_url[:index+4]
                     print(mod_link)
+                    return mod_link
                 
+def on_confirm():
+    driver.get(entry_link.get()) 
+    download_link_title = tk.Label(window, text="Download link:")
+    download_link_title.pack()
+    field_with_lk = tk.Entry(window, width=50)
+    field_with_lk.pack()
+    def copy_link():
+        link = field_with_lk.get()
+        clipboard.copy(link)
+        messagebox.showinfo("Link Copied", "The link has been copied to the clipboard.")
+    button_copy = tk.Button(window, text="Copy Link", command=copy_link)
+    button_copy.pack()
+    
+    
+    end_time = datetime.now() + timedelta(seconds=2)
+    while datetime.now() < end_time:
+        logs = driver.get_log("performance")
+        process_browser_logs_for_network_events(logs)
+    driver.quit()
+    mod_link = find_url()
+    field_with_lk.insert(0, mod_link)
 
-driver.get("link") #link to the website from which you want to download video content
+window = tk.Tk()
+window.geometry("400x300")
+window.title("Download-HDrezka")
 
-logs = driver.get_log("performance")
+# Create the link label and entry field
+label_link = tk.Label(window, text="Link to what you want to download:")
+label_link.pack()
+entry_link = tk.Entry(window, width=50)
+entry_link.pack()
 
-process_browser_logs_for_network_events(logs)
-find_url()
+# Create the download button
+button_confirm = tk.Button(window, text="Get download link", command=on_confirm)
+button_confirm.pack()
 
+# Start the Tkinter event loop
+window.mainloop()
 
 
